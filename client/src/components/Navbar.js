33c5975ca2ca3b4c4bf4c8a0727bb580648f5a52
Ely Karr <elykarr@gmail.com>
2022-05-02 22:02:20 -0700

    Working on navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Navbar = ({ user, setUser }) => {
+
     function handleLogoutClick() {
         fetch("/logout", { method: "DELETE" }).then((r) => {
             if (r.ok) {
@@ -9,12 +10,26 @@ const Navbar = ({ user, setUser }) => {
             }
         });
     }
+
+    const loggedInLinks = () => {
+        <div className='NavbarDiv'>
+        </div>
+
+    }
+
+    const loggedOutLinks = () => {
+        <div className='NavbarDiv'>
+            <Link to='/'>Home</Link>
+            <button onClick={handleLogoutClick}>Logout</button>
+        </div>
+
+    }
+
+
     return (
+
         <div>
-            <div className='NavbarDiv'>
-                <Link to='/'>Home</Link>
-                <button onClick={handleLogoutClick}>Logout</button>
-            </div>
+
         </div>
     )
 }
