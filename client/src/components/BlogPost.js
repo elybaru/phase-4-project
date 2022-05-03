6bd8fe217454695c0ed941c2637492fab994eccb
Ely Karr <elykarr@gmail.com>
2022-05-02 22:13:37 -0700

    creating blog post component

---
+++ client/src/components/BlogPost.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+
+const BlogPost = () => {
+    // receive from fetch or props the blog post, with the comments
+    // map our comments to child component- or how should that be structured, conditional rendering if there are further child components in the object?
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default BlogPost
