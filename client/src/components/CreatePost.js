cf701119ec39d9e79f8a7d0a6cfd073d29f6f29f
Ely Karr <elykarr@gmail.com>
2022-05-02 22:15:41 -0700

    Create a new entry component

---
+++ client/src/components/CreatePost.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+
+const CreatePost = () => {
+    // stateful component for the create form
+    // fetch request for POST of the form
+    // how to interact with current user, must receive it in props?
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default CreatePost
