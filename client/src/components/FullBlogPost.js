580b186866bda3151645cbd11b0d96d6a60beffa
Ely Karr <elykarr@gmail.com>
2022-05-04 15:34:43 -0700

    latest

--- client/src/components/FullBlogPost.js
+++ client/src/components/FullBlogPost.js
@@ -1,8 +1,43 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import {useParams} from 'react-router-dom'
 
 const FullBlogPost = () => {
+    const [post, setPost] = useState(null)
+
+    let { id } = useParams()
+    const [posts, setPosts] = useState()
+
+    useEffect(() => {
+
+        fetch(`/posts/${id}/`).then((r) => {
+            if (r.ok) {
+                r.json().then((data) => setPosts(data));
+            }
+        });
+    }, []);
+    
+    console.log(post)
+
     return (
         <div>
+            I am a full blog post
+            {post ? 
+            <div>
+                
+                <h2>{post.title}</h2>
+                <div>
+                    {post.content}
+                </div>
+                <div>
+                    <form className="main-comment-form">
+                    <input type='submit' onSubmit={handleSubmit} />
+                    </form>    
+                </div>
+                <div>{post.comments ? post.comments.map : ""}</div>
+            </div>
+
+                : ""
+            }
             
         </div>
     )
